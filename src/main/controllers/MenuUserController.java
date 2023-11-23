f4bfdf80c5e054a30d843a87ef7f0aec31f1a410
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-23 22:37:27 +0100

    Ajout majeur d'entrées dans le menu (7 nouvelles entrées)

--- src/main/controllers/MenuUserController.java
+++ src/main/controllers/MenuUserController.java
@@ -1,7 +1,15 @@
 package main.controllers;
 
 import main.common.GFG;
-
+import main.models.ZooFantastique;
+import main.models.creatures.Creature;
+import main.models.creatures.implemente.*;
+import main.models.enclos.Enclos;
+import main.models.enclos.implemente.Aquarium;
+import main.models.enclos.implemente.Cage;
+import main.models.enclos.implemente.Voliere;
+import java.util.ArrayList;
+import static main.common.Check.checkIfEntreeIsInt;
 import static main.controllers.DefaultController.*;
 
 public class MenuUserController {
@@ -9,6 +17,7 @@ public class MenuUserController {
         int exit = 0;
         while (exit==0) {
             int menuOutput = menuView.userMenu();
+            int entree;
             switch (menuOutput) {
                 case 0:
                     exit=1;
@@ -19,9 +28,9 @@ public class MenuUserController {
                     // Affichage de toutes les créatures dans le Zoo
                     if (!zooFantastique.getToutesCreatureDansZoo().isEmpty()) {
                         for (int i = 0; i<zooFantastique.getToutesCreatureDansZoo().size(); ++i){
-                            System.out.println("Créature " + (i+1) + " : " + zooFantastique.getToutesCreatureDansZoo().get(i).getNom());
+                            System.out.println("\tCréature " + (i+1) + " : " + zooFantastique.getToutesCreatureDansZoo().get(i).getNom());
                         }
-                        System.out.println("\n");
+                        System.out.println();
                     } else {
                         System.out.println("Pas de créatures\n");
                     }
@@ -31,53 +40,161 @@ public class MenuUserController {
                     System.out.println(zooFantastique.getToutesCreatureDansZoo().size()+"\n");
                     break;
                 case 3:
-                    // Affiche des enclos dans le Zoo
+                    // Examiner une créature
+                    menuView.afficherChoixCreatures();
+                    try {
+                        maitreZoo.examinerCreature(Creature.InstanceManager.getAllInstances().get(checkIfEntreeIsInt()-1));
+                    } catch (Exception e) {
+                        menuView.numeroEntreeInvalideErrorMessage();
+                    }
+                    break;
+                case 4:
+                    // Affiche les enclos dans le Zoo
                     if (!zooFantastique.getEnclosExistants().isEmpty()) {
                         for (int i = 0; i<zooFantastique.getEnclosExistants().size(); ++i){
-                            System.out.println("Enclos " + (i+1) + " : " + zooFantastique.getEnclosExistants().get(i).getNom());
+                            System.out.println("\tEnclos " + (i+1) + " : " + zooFantastique.getEnclosExistants().get(i).getNom());
                         }
-                        System.out.println("\n");
+                        System.out.println();
                     } else {
                         System.out.println("Pas d'enclos\n");
                     }
                     break;
-                case 4:
+                case 5:
                     // Affichage du nombre d'enclos
                     System.out.println(zooFantastique.getEnclosExistants().size()+"\n");
                     break;
-                case 5:
+                case 6:
                     // Examiner un enclos
                     try {
-                        maitreZoo.examinerEnclos(zooFantastique.getEnclosExistants().get(menuView.checkIfEntreeIsInt()-1));
+                        maitreZoo.examinerEnclos(zooFantastique.getEnclosExistants().get(checkIfEntreeIsInt()-1));
                     } catch (Exception e) {
-                        System.out.println(GFG.ANSI_YELLOW+"Vous n'avez pas entré un numéro d'enclos valide...\n"+GFG.ANSI_RESET);
-                    }
-                    break;
-                case 6:
-                    // Nettoyer un enclos
-                    try {
-                        maitreZoo.nettoyerEnclos(zooFantastique.getEnclosExistants().get(menuView.checkIfEntreeIsInt()-1));
-                    } catch (Exception e) {
-                        System.out.println(GFG.ANSI_YELLOW+"Vous n'avez pas entré un numéro d'enclos valide...\n"+GFG.ANSI_RESET);
+                        menuView.numeroEntreeInvalideErrorMessage();
                     }
                     break;
                 case 7:
-                    // Nourrir créatures dans un enclos
+                    // Nettoyer un enclos
                     try {
-                        maitreZoo.nourrirEnclos(zooFantastique.getEnclosExistants().get(menuView.checkIfEntreeIsInt()-1));
+                        maitreZoo.nettoyerEnclos(zooFantastique.getEnclosExistants().get(checkIfEntreeIsInt()-1));
                     } catch (Exception e) {
-                        System.out.println(GFG.ANSI_YELLOW+"Vous n'avez pas entré un numéro d'enclos valide...\n"+GFG.ANSI_RESET);
+                        menuView.numeroEntreeInvalideErrorMessage();
                     }
                     break;
                 case 8:
+                    // Nourrir créatures dans un enclos
+                    try {
+                        maitreZoo.nourrirEnclos(zooFantastique.getEnclosExistants().get(checkIfEntreeIsInt()-1));
+                    } catch (Exception e) {
+                        menuView.numeroEntreeInvalideErrorMessage();
+                    }
+                    break;
+                case 9:
+                    // Affichage des données du maitre de zoo et du zoo
                     System.out.println(maitreZoo);
                     System.out.println(zooFantastique);
                     break;
-                case 9:
-                    System.out.println();
-                    break;
                 case 10:
-                    System.out.println(" ");
+                    // Modification des information du maitre du zoo
+                    int choix = checkIfEntreeIsInt();
+                    if (choix == 1) {
+                        maitreZoo.setNom(modificationEntiteView.changeInformationMaitreZoo(choix));
+                    } else if (choix == 2) {
+                        maitreZoo.setSexe(Integer.parseInt(modificationEntiteView.changeInformationMaitreZoo(choix)));
+                    } else if (choix == 3) {
+                        maitreZoo.setAge(Integer.parseInt(modificationEntiteView.changeInformationMaitreZoo(choix)));
+                    } else {
+                        menuView.numeroEntreeInvalideErrorMessage();
+                    }
+                    break;
+                case 11:
+                    // Changer le nom de zoo
+                    zooFantastique.setNom(modificationEntiteView.changerNomZoo());
+                    break;
+                case 12:
+                    // Nourrir seulement une créature
+                    menuView.afficherChoixCreatures();
+                    try {
+                        Creature.InstanceManager.getAllInstances().get(checkIfEntreeIsInt()-1).manger();
+                        System.out.println();
+                    } catch (Exception e) {
+                        menuView.numeroEntreeInvalideErrorMessage();
+                    }
+                    break;
+                case 13:
+                    entree = checkIfEntreeIsInt();
+                    ArrayList<String> listeInformationCreature = ajoutEntiteView.ajoutCreature();
+                    switch (entree) {
+                        case 1:
+                            listCreatureInitialise.add(new Dragon(listeInformationCreature.get(0),Integer.parseInt(listeInformationCreature.get(1)),
+                                    Integer.parseInt(listeInformationCreature.get(2)),Integer.parseInt(listeInformationCreature.get(3)),
+                                    Integer.parseInt(listeInformationCreature.get(4))));
+                            break;
+                        case 2:
+                            listCreatureInitialise.add(new Krakens(listeInformationCreature.get(0),Integer.parseInt(listeInformationCreature.get(1)),
+                                    Integer.parseInt(listeInformationCreature.get(2)),Integer.parseInt(listeInformationCreature.get(3)),
+                                    Integer.parseInt(listeInformationCreature.get(4))));
+                            break;
+                        case 3:
+                            listCreatureInitialise.add(new Licornes(listeInformationCreature.get(0),Integer.parseInt(listeInformationCreature.get(1)),
+                                    Integer.parseInt(listeInformationCreature.get(2)),Integer.parseInt(listeInformationCreature.get(3)),
+                                    Integer.parseInt(listeInformationCreature.get(4))));
+                            break;
+                        case 4:
+                            listCreatureInitialise.add(new Lycanthropes(listeInformationCreature.get(0),Integer.parseInt(listeInformationCreature.get(1)),
+                                    Integer.parseInt(listeInformationCreature.get(2)),Integer.parseInt(listeInformationCreature.get(3)),
+                                    Integer.parseInt(listeInformationCreature.get(4))));
+                            break;
+                        case 5:
+                            listCreatureInitialise.add(new Megalodons(listeInformationCreature.get(0),Integer.parseInt(listeInformationCreature.get(1)),
+                                    Integer.parseInt(listeInformationCreature.get(2)),Integer.parseInt(listeInformationCreature.get(3)),
+                                    Integer.parseInt(listeInformationCreature.get(4))));
+                            break;
+                        case 6:
+                            listCreatureInitialise.add(new Nymphes(listeInformationCreature.get(0),Integer.parseInt(listeInformationCreature.get(1)),
+                                    Integer.parseInt(listeInformationCreature.get(2)),Integer.parseInt(listeInformationCreature.get(3)),
+                                    Integer.parseInt(listeInformationCreature.get(4))));
+                            break;
+                        case 7:
+                            listCreatureInitialise.add(new Phenix(listeInformationCreature.get(0),Integer.parseInt(listeInformationCreature.get(1)),
+                                    Integer.parseInt(listeInformationCreature.get(2)),Integer.parseInt(listeInformationCreature.get(3)),
+                                    Integer.parseInt(listeInformationCreature.get(4))));
+                            break;
+                        case 8:
+                            listCreatureInitialise.add(new Sirenes(listeInformationCreature.get(0),Integer.parseInt(listeInformationCreature.get(1)),
+                                    Integer.parseInt(listeInformationCreature.get(2)),Integer.parseInt(listeInformationCreature.get(3)),
+                                    Integer.parseInt(listeInformationCreature.get(4))));
+                            break;
+                    }
+                    // TODO: ajouté les créature créé a des enclos
+                    break;
+
+                case 14:
+                    if (Enclos.InstanceManager.getAllInstances().size()+1 <= ZooFantastique.getNombreMaxEnclos()) {
+                        menuView.menuCreationEnclos();
+                        entree = checkIfEntreeIsInt();
+                        ArrayList<String> listInformationNouvelEnclos;
+                        switch (entree) {
+                            case 1:
+                                listInformationNouvelEnclos = ajoutEntiteView.ajoutEnclos();
+                                listEnclosInitialise.add(new Cage(listInformationNouvelEnclos.get(0), Integer.parseInt(listInformationNouvelEnclos.get(1)),
+                                        Integer.parseInt(listInformationNouvelEnclos.get(2))));
+                                System.out.println();
+                                break;
+                            case 2:
+                                listInformationNouvelEnclos = ajoutEntiteView.ajoutEnclosAquarium();
+                                listEnclosInitialise.add(new Aquarium(listInformationNouvelEnclos.get(0), Integer.parseInt(listInformationNouvelEnclos.get(1)),
+                                        Integer.parseInt(listInformationNouvelEnclos.get(2)), Integer.parseInt(listInformationNouvelEnclos.get(3))));
+                                System.out.println();
+                                break;
+                            case 3:
+                                listInformationNouvelEnclos = ajoutEntiteView.ajoutEnclosVoliere();
+                                listEnclosInitialise.add(new Voliere(listInformationNouvelEnclos.get(0), Integer.parseInt(listInformationNouvelEnclos.get(1)),
+                                        Integer.parseInt(listInformationNouvelEnclos.get(2)), Integer.parseInt(listInformationNouvelEnclos.get(3))));
+                                System.out.println();
+                                break;
+                        }
+                    } else {
+                        System.out.println(GFG.ANSI_YELLOW+"La capacité maximum du zoo est atteinte..."+GFG.ANSI_RESET+"\n");
+                    }
                     break;
             }
         }
