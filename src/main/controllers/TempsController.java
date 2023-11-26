2e2612d59bd8b5a2c351bb2cdf6f388a8fbaa27f
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-26 23:54:11 +0100

    Ajout de l'incubation des oeufs + suppression affichage passage jours/mois/années

--- src/main/controllers/TempsController.java
+++ src/main/controllers/TempsController.java
@@ -12,10 +12,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 public class TempsController {
     private final Temps temps;
-    private static final Map<ArrayList<Creature>, Integer> moisDepuisDerniereNaissanceMap = new HashMap<>();
+    private static final Map<ArrayList<Creature>, Integer> moisDeGestationMap = new HashMap<>();
+    private static final Map<ArrayList<Creature>, Integer> joursDIncubationMap = new HashMap<>();
+
     public TempsController(ZooFantastique zooFantastique) {
         this.temps = new Temps(zooFantastique);
     }
@@ -30,67 +31,92 @@ public class TempsController {
         }
     }
 
-    public static void nouvelleNaissance(Creature pere, Creature mere) {
+    public static void nouvelleNaissanceVivipare(Creature pere, Creature mere) {
+        ajouterNaissance(pere, mere, moisDeGestationMap, 9);
+    }
+
+    public static void nouvelleNaissanceOvipare(Creature pere, Creature mere) {
+        ajouterNaissance(pere, mere, joursDIncubationMap, 21);
+    }
+
+    private static void ajouterNaissance(Creature pere, Creature mere, Map<ArrayList<Creature>, Integer> moisMap, int conditionNaissance) {
         ArrayList<Creature> pereEtMere = new ArrayList<>();
         pereEtMere.add(pere);
         pereEtMere.add(mere);
-        moisDepuisDerniereNaissanceMap.put(pereEtMere, 0);
+        moisMap.put(pereEtMere, 0);
     }
 
-    protected void ajouterUnAnAToutLeMonde() {
+    // Cette fonction va incrémenté les mois/jours depuis l'ovulation et va vérifier si la gestation ou l'incubation est arrivé a terme
+    private void checkNaissanceGeneric(Map<ArrayList<Creature>, Integer> tempsDepuisFecondationMap, String eventType, int tempsAttenteAvantNaissance) {
+        List<ArrayList<Creature>> creaturesAEnlever = new ArrayList<>();
+        for (Map.Entry<ArrayList<Creature>, Integer> entry : tempsDepuisFecondationMap.entrySet()) {
+            Creature pere = entry.getKey().get(0);
+            int moisDepuisDerniereNaissance = entry.getValue();
+            moisDepuisDerniereNaissance++;
+
+            if (moisDepuisDerniereNaissance >= tempsAttenteAvantNaissance) {
+                String prefixMessage = eventType.equals("gestation") ? "" : "L'oeuf de ";
+                String suffixeMessage = eventType.equals("gestation") ? "a accouché" : "a éclot";
+                System.out.println(prefixMessage + entry.getKey().get(1).getNom() + " " + suffixeMessage + ", créer son bébé");
+                Check.checkEspeceEtAjoutCreaturePourNaissance(pere.getEspece());
+                Creature creatureNee = Creature.InstanceManager.getAllInstances().get(Creature.InstanceManager.getAllInstances().size() - 1);
+                creatureNee.getEstEnfantDe().add(entry.getKey().get(0));
+                creatureNee.getEstEnfantDe().add(entry.getKey().get(1));
+                entry.getKey().get(0).getEstParentDe().add(creatureNee);
+                entry.getKey().get(1).getEstParentDe().add(creatureNee);
+                Creature.InstanceManager.addCreatureNee(creatureNee);
+                System.out.println("Nouvelle naissance !\n");
+                creaturesAEnlever.add(entry.getKey());
+            }
+            tempsDepuisFecondationMap.put(entry.getKey(), moisDepuisDerniereNaissance);
+        }
+
+        // Supprimer les entrées pour les créatures qui ont donné naissance
+        for (ArrayList<Creature> creaturePair : creaturesAEnlever) {
+            tempsDepuisFecondationMap.remove(creaturePair);
+        }
+    }
+
+    protected void checkMoisGestation() {
+        checkNaissanceGeneric(moisDeGestationMap, "gestation", 9);
+    }
+
+    protected void checkMoisIncubation() {
+        checkNaissanceGeneric(joursDIncubationMap, "incubation", 21);
+    }
+
+    // ... (autres méthodes inchangées)
+
+    private void ajouterUnAnAToutLeMonde() {
         MaitreZoo maitreZoo = temps.getZooFantastique().getMaitreZoo();
         for (Creature creature : Creature.InstanceManager.getAllInstances()) {
             creature.vieillir(1);
         }
-        maitreZoo.setAge(maitreZoo.getAge()+1);
+        maitreZoo.setAge(maitreZoo.getAge() + 1);
     }
 
     public void ajouterUnMois() {
-        if (!moisDepuisDerniereNaissanceMap.isEmpty()) {
-            List<ArrayList<Creature>> creaturesAEnlever = new ArrayList<>();
-            for (Map.Entry<ArrayList<Creature>, Integer> entry : moisDepuisDerniereNaissanceMap.entrySet()) {
-                Creature pere = entry.getKey().getFirst();
-                int moisDepuisDerniereNaissance = entry.getValue();
-                moisDepuisDerniereNaissance++;
-                System.out.println(moisDepuisDerniereNaissance);
-
-                if (moisDepuisDerniereNaissance >= 9) {
-                    System.out.println(entry.getKey().getLast().getNom()+" a accouché, créer son bébé");
-                    Check.checkEspeceEtAjoutCreaturePourNaissance(pere.getEspece());
-                    Creature creatureNee = Creature.InstanceManager.getAllInstances().getLast();
-                    creatureNee.getEstEnfantDe().add(entry.getKey().getFirst());
-                    creatureNee.getEstEnfantDe().add(entry.getKey().getLast());
-                    entry.getKey().getFirst().getEstParentDe().add(creatureNee);
-                    entry.getKey().getLast().getEstParentDe().add(creatureNee);
-                    Creature.InstanceManager.addCreatureNee(creatureNee);
-                    System.out.println("Nouvelle naissance !");
-                    creaturesAEnlever.add(entry.getKey());
-                }
-                moisDepuisDerniereNaissanceMap.put(entry.getKey(), moisDepuisDerniereNaissance);
-            }
-            // Supprimer les entrées pour les créatures qui ont donné naissance
-            for (ArrayList<Creature> creaturePair : creaturesAEnlever) {
-                moisDepuisDerniereNaissanceMap.remove(creaturePair);
-            }
+        if (!moisDeGestationMap.isEmpty()) {
+            checkMoisGestation();
         }
-        if (Temps.getMois()<12) {
-            temps.setMois(Temps.getMois()+1);
-            System.out.println("Le mois est passé");
+        if (Temps.getMois() < 12) {
+            temps.setMois(Temps.getMois() + 1);
         }
     }
 
     public void ajouterUneAnnee() {
         temps.setJour(1);
         temps.setMois(1);
-        temps.setAnnee(Temps.getAnnee()+1);
+        temps.setAnnee(Temps.getAnnee() + 1);
         ajouterUnAnAToutLeMonde();
-        System.out.println("L'année est passé");
     }
 
     public void passeUnJour() {
         if (Temps.getJour() < 31) {
-            temps.setJour(Temps.getJour()+1);
-            System.out.println("La journée est passé");
+            temps.setJour(Temps.getJour() + 1);
+            if (!joursDIncubationMap.isEmpty()) {
+                checkMoisIncubation();
+            }
         } else {
             temps.setJour(1);
             if (Temps.getMois() < 12) {
