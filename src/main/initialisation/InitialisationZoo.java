32b30fa7e1a6e0d91d0a474d73e0ff946196b229
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-11-29 11:06:46 +0100

    Ajout des enclos et des commentaires de l'initialisation

--- src/main/initialisation/InitialisationZoo.java
+++ src/main/initialisation/InitialisationZoo.java
@@ -42,21 +42,26 @@ public class InitialisationZoo {
 
     public static void initializeDonneesDuJeu() {
         // Initialisation des enclos
-        listEnclosInitialise.add(new Cage("Premier enclos", 34, 6));
-        listEnclosInitialise.add(new Cage("Deuxième enclos", 34, 6));
+        listEnclosInitialise.add(new Cage("Enclos de Licornes", 34, 6));
+        listEnclosInitialise.add(new Cage("Enclos de Dragons", 34, 6));
+        listEnclosInitialise.add(new Cage("Enclos de Krakens", 34, 6));
 
         // Initialisation des créatures
         listCreatureInitialise.add(new Dragon("Papa Dragon", 13, 0, 300, 4));
         listCreatureInitialise.add(new Dragon("Maman Dragonne", 13, 1, 300, 4));
-        // Initialisation du "Dragon" avec une faim a 80% et ses deux parents
+
+        // Initialisation du "Dragon" avec une faim à 80% et ses deux parents
         listCreatureInitialise.add(new Dragon("Dragon", 13, 0, 300, 4));
         listCreatureInitialise.get(2).setIndicateurFaim(80);
         listCreatureInitialise.get(2).getEstEnfantDe().add(listCreatureInitialise.get(0));
         listCreatureInitialise.get(2).getEstEnfantDe().add(listCreatureInitialise.get(1));
+
         // Initialisation de la "Dragonne" avec ses deux parents
         listCreatureInitialise.add(new Dragon("Dragonne", 13, 1, 300, 4));
         listCreatureInitialise.get(3).getEstEnfantDe().add(listCreatureInitialise.get(0));
         listCreatureInitialise.get(3).getEstEnfantDe().add(listCreatureInitialise.get(1));
+
+        // Initialisation des autres créatures
         listCreatureInitialise.add(new Krakens("Krakens", 13, 0, 300, 4));
         listCreatureInitialise.add(new Licornes("Licorne mâle", 13, 0, 300, 4));
         listCreatureInitialise.add(new Licornes("Licorne femelle", 13, 1, 300, 4));
@@ -67,10 +72,17 @@ public class InitialisationZoo {
         listCreatureInitialise.get(1).getEstParentDe().add(listCreatureInitialise.get(2));
         listCreatureInitialise.get(1).getEstParentDe().add(listCreatureInitialise.get(3));
 
-        // AJout des créatures dans l'enclos 0 Premier enclos
+        // AJout des créatures dans l'enclos "Enclos de Licornes"
+        listEnclosInitialise.get(0).ajouterCreature(listCreatureInitialise.get(5));
+        listEnclosInitialise.get(0).ajouterCreature(listCreatureInitialise.get(6));
+
+        // AJout des créatures dans l'enclos "Enclos de Dragons"
         listEnclosInitialise.get(1).ajouterCreature(listCreatureInitialise.get(0));
         listEnclosInitialise.get(1).ajouterCreature(listCreatureInitialise.get(1));
         listEnclosInitialise.get(1).ajouterCreature(listCreatureInitialise.get(2));
         listEnclosInitialise.get(1).ajouterCreature(listCreatureInitialise.get(3));
+
+        // AJout des créatures dans l'enclos "Enclos de Licornes"
+        listEnclosInitialise.get(2).ajouterCreature(listCreatureInitialise.get(4));
     }
 }
